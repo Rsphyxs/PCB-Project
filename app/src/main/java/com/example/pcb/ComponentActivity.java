2b80a9f08b0252dc31443feda7e270eb664c945e
Rsphyxs <muhammad.ilham92@ui.ac.id>
2021-11-25 20:27:07 +0700

    PC Component Activity

---
+++ app/src/main/java/com/example/pcb/ComponentActivity.java
@@ -0,0 +1,14 @@
+package com.example.pcb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComponentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_component);
+    }
+}
